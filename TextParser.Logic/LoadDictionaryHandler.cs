e8f483c62f227ee780ce2df0035ac76a590e33ab
Massine Mouha <massinemouha@gmail.com>
2022-08-22 12:44:55 +0100

    Extract keys and values from dictionary

--- TextParser.Logic/LoadDictionaryHandler.cs
+++ TextParser.Logic/LoadDictionaryHandler.cs
@@ -1,6 +1,34 @@
 namespace TextParser.Logic;
 
-public class LoadDictionaryHandler
+public record LoadDictionaryRequest(IEnumerable<string> Lines);
+
+public static class LoadDictionaryHandler
 {
-    
+    public static Dictionary<string, string> Do(LoadDictionaryRequest request)
+    {
+        Dictionary<string, string> dictionary = new();
+
+        // check all dictionary lines
+        foreach (var line in request.Lines)
+        {
+            // split by definition symbol
+            var data = line.Split(':');
+
+            // get the key and trim exess space
+            var key = data[0].Trim();
+            
+            // get value and remove excess space
+            var value = data[1].Trim();
+
+            // If value is wrapped with "" we remove them and extract the inner value
+            if (value.StartsWith('"') && value.EndsWith('"'))
+                value = value[1..^1];
+
+            // adding key and value to dictionary
+            dictionary.Add(key, value);
+        }
+
+        // returning the dictionary
+        return dictionary;
+    }
 }
