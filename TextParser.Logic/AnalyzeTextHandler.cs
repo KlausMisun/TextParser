1b3770063bbf43159936f24a25c3a1edef73c827
Massine Mouha <massinemouha@gmail.com>
2022-08-22 12:42:42 +0100

    Analyzes the text and decides what operations to do, the logic to detect flags and operations to disable should be modified to be able to expand later on

--- TextParser.Logic/AnalyzeTextHandler.cs
+++ TextParser.Logic/AnalyzeTextHandler.cs
@@ -1,6 +1,76 @@
+using TextParser.Models.Constants;
+using TextParser.Models.Data;
+using TextParser.Models.Enums;
+
 namespace TextParser.Logic;
+
+public record AnalyzeTextRequest(IEnumerable<TextChunk> Chunks);
 
 public class AnalyzeTextHandler
 {
-    
+    public static IEnumerable<TextProcessingDetails> Do(AnalyzeTextRequest request)
+    {
+        var processingDetails = new List<TextProcessingDetails>();
+        foreach (var chunk in request.Chunks)
+        {
+            var flag = chunk.Text
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .FirstOrDefault();
+            
+            var operation = ParseOperation.None;
+
+            if (chunk.Parse && flag is not null)
+            {
+                // finding the elements to disable 
+                var disableKeywords = flag.Contains(Symbols.DisableKeywordConversion);
+                var disableFonts = flag.Contains(Symbols.DisableFontConversion);
+
+                // checking the operations the code can handle
+                operation = flag switch
+                {
+                    _ when  disableKeywords && disableFonts
+                        => ParseOperation.None,
+                    _ when disableKeywords
+                        => ParseOperation.Font,
+                    _ when disableFonts
+                        => ParseOperation.KeyWords,
+                    _ => ParseOperation.All
+                };
+
+                // checking the length of the used flags
+                var flagsLength = operation switch
+                {
+                    ParseOperation.KeyWords =>
+                        Symbols.DisableFontConversion.Length,
+                    
+                    ParseOperation.Font =>
+                        Symbols.DisableKeywordConversion.Length,
+                    
+                    ParseOperation.None =>
+                        Symbols.DisableKeywordConversion.Length
+                        + Symbols.DisableFontConversion.Length,
+                    
+                    _ => 0
+                };
+                
+                // removing all excess in the text
+                if (flagsLength != 0 && chunk.Text.Length >= flagsLength)
+                {
+                    chunk.Text = chunk.Text[flagsLength..];
+                }
+
+                //Removing unneeded white space
+                chunk.Text = chunk.Text.Trim();
+            }
+
+            // Details to add on shit
+            processingDetails.Add(new TextProcessingDetails
+            {
+                Text = chunk.Text,
+                Operation = operation,
+            });
+        }
+        
+        return processingDetails;
+    }
 }
