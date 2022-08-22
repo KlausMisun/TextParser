de3522c1792020f6d89c5442fc4107b876074be3
Massine Mouha <massinemouha@gmail.com>
2022-08-22 12:43:55 +0100

    Keyword transformation similar to font transformation

--- TextParser.Logic/KeywordTransformHandler.cs
+++ TextParser.Logic/KeywordTransformHandler.cs
@@ -1,6 +1,18 @@
+using System.Text;
+
 namespace TextParser.Logic;
 
-public class KeywordTransform
+public record KeywordTransformRequest(string Text, Dictionary<string, string> Keywords);
+
+public class KeywordTransformHandler
 {
-    
+    public static string Do(KeywordTransformRequest request)
+    {
+        var sb = new StringBuilder(request.Text);
+
+        foreach (var key in request.Keywords.Keys)
+            sb.Replace(key, request.Keywords[key]);
+        
+        return sb.ToString();
+    }
 }
