84e4abdda25f9047896e364c44aedb8bbbeba2bb
Massine Mouha <massinemouha@gmail.com>
2022-08-22 12:40:40 +0100

    Interface to handle File access and reading, to be edited later

--- TextParser.Logic/Interfaces/IFileManager.cs
+++ TextParser.Logic/Interfaces/IFileManager.cs
@@ -2,5 +2,8 @@ namespace TextParser.Logic.Interfaces;
 
 public interface IFileManager
 {
-    
+    string ReadFile(string path);
+    IEnumerable<string> ReadFileLines(string path);
+    IEnumerable<string> ReadFontLines();
+    IEnumerable<string> ReadKeywordLines();
 }
