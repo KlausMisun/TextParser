60bf01a00c0e79b55ee9cfce46b98ca7dc680acb
Massine Mouha <massinemouha@gmail.com>
2022-08-22 12:43:15 +0100

    Transforms the Letters inside the text to parse into unicode characters

--- TextParser.Logic/FontTransformHandler.cs
+++ TextParser.Logic/FontTransformHandler.cs
@@ -1,6 +1,17 @@
+using System.Text;
+
 namespace TextParser.Logic;
 
-public class FontTransform
+public record FontTransformRequest(string Text, Dictionary<string, string> Font);
+
+public class FontTransformHandler
 {
-    
+    public static string Do(FontTransformRequest request)
+    {
+        var sb = new StringBuilder(request.Text);
+        foreach (var letter in request.Font.Keys)
+            sb.Replace(letter, request.Font[letter]);
+        
+        return sb.ToString();
+    }
 }
