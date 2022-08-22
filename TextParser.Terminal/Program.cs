8aeabdeb99c8d03f7eb9d421ce40e98eb4636c6c
Massine Mouha <massinemouha@gmail.com>
2022-08-22 12:48:57 +0100

    Terminal Code, underwork as to grow the data

---
+++ TextParser.Terminal/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TextParser.Infrastructure;
+using TextParser.Logic;
+
+Console.WriteLine("Hello, World!");
+var fileInput = File.ReadAllText(args[0]);
+
+var result = new ParseTextHandler(new FileManager()).Do(new ParseTextRequest(fileInput));
+
+Console.WriteLine(result);
